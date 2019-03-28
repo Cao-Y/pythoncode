2d7db2adeb4adb9ac6f9fc1809e1893d12fe558e
first use <715316146@qq.com>
2019-03-28 23:07:38 +0800

    0328pyqt5,update use camera,first learn net

--- learnQt5/camera/useCamera.py
+++ learnQt5/camera/useCamera.py
@@ -12,7 +12,9 @@ class my_Window(QMainWindow,Ui_MainWindow): #继承Qt Designer设计的ui
         super(my_Window, self).__init__()
         self.setupUi(self)
         self.cap = cv2.VideoCapture(0) #获取0号摄像头
-        self.cap2 = cv2.VideoCapture(2)  #获取2号摄像头
+        self.textBrowser.append('摄像头1\n')
+        self.cap2 = cv2.VideoCapture(1)  #获取2号摄像头
+        self.textBrowser.append('摄像头2')
         self.timer = QtCore.QTimer()
         self.timer.start()
         self.timer.setInterval(100)
