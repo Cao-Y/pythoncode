5fcc44c1a93b1edae2aa3c373098a3a81a7de4bd
first use <715316146@qq.com>
2019-04-09 21:43:13 +0800

    0409 update huaPen and code

--- learnQt5/huaPenCode.py
+++ learnQt5/huaPenCode.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import sys
 from PyQt5.QtWidgets import QApplication,QMainWindow
 from PyQt5 import QtCore,QtGui
@@ -13,18 +15,21 @@ class my_Window(QMainWindow,Ui_MainWindow):
         super(my_Window, self).__init__()
         self.setupUi(self)
         self.cap = cv2.VideoCapture(0) #获取0号摄像头
+        self.command = 'a'  # 传输给控制板的命令
+        self.message = []
         self.timer = QtCore.QTimer()
         self.timer.start()
         self.timer.setInterval(100)
         self.timer.timeout.connect(self.start)    #0号摄像头开始使用
+        self.textEdit.setPlainText("Start to use")
+        self.displayMessage()
         self.lineEdit.textChanged['QString'].connect(self.receve)
         self.Button.clicked.connect(self.displayMessage)  #点击按钮发送信息给控制板
         self.Button.clicked.connect(self.displayText) #点击按钮在文本框中显示信息
-        self.command =''   #文本框输入的命令
         self.show()
 
     def displayText(self):  #设置文本框显示信息
-        self.textEdit.setPlainText("开始使用")
+        self.textEdit.append("更新信息")
 
 
     def receve(self):  #获得文本框输入信息
@@ -45,8 +50,8 @@ class my_Window(QMainWindow,Ui_MainWindow):
 
     def start(self):
         flag, image = self.cap.read()  #读取摄像头
-        #show1 = cv2.resize(image, (640, 480))
-        show = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  #处理读取的图像信息
+        show = cv2.resize(image, (200, 200))
+        show = cv2.cvtColor(show, cv2.COLOR_BGR2RGB)  #处理读取的图像信息
         showImage = QtGui.QImage(show.data, show.shape[1], show.shape[0], QtGui.QImage.Format_RGB888)
         self.label_5.setPixmap(QtGui.QPixmap.fromImage(showImage))  # 摄像头信息传给label
 
@@ -62,11 +67,13 @@ class my_Window(QMainWindow,Ui_MainWindow):
 
     def displayMessage(self):  #获得信息，使用正则表达式处理信息，将信息显示在界面上
         allMessage = self.recevemessage()  #收到信息
-        print(allMessage)
+        #print(allMessage)
         a = re.compile(r'(?<=:).*?(?=@)')  # 正则表达式截取信息
         message = a.findall(allMessage.decode('utf-8'))
         for i in range(6):  #处理数据 str-> float
             message[i] =float(message[i])
+
+        self.displayText(message)
         '''将信息显示在界面上'''
         self.lcdNumber.display(message[0])
         self.lcdNumber_2.display(message[1])
@@ -74,6 +81,13 @@ class my_Window(QMainWindow,Ui_MainWindow):
         self.lcdNumber_4.display(message[3])
         self.lcdNumber_5.display(message[4])
         self.lcdNumber_6.display(message[5])
+    def displayText(self,message):
+        if(message[1]) > 30:
+            self.textEdit.append("The flowers are thirsty")
+        if(message[4]) > 40:
+            self.textEdit.append("The flowers are sunburned")
+
+
 
 
 if __name__ == '__main__':
